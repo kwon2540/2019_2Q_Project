3be753115319cde941a6ccec666be67c47deabfc
JUNHYEOK KWON <>
2019-11-16 14:23:15 +0900

    Create base file

---
+++ Project_2Q_2019/Project_2Q_2019/Screen/Splash/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  Project_2Q_2019
+//
+//  Created by JUNHYEOK KWON on 2019/11/16.
+//  Copyright © 2019 JUNHYEOK KWON. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
