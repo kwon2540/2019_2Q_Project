bdec1bc6a0c0c006542a926d9d1652e21e961aa9
JUNHYEOK KWON <>
2019-11-30 16:54:01 +0900

    콜렉션뷰 기본 세팅

--- Project_2Q_2019/Project_2Q_2019/Screen/Home/HomeViewController.swift
+++ Project_2Q_2019/Project_2Q_2019/Screen/Home/HomeViewController.swift
@@ -15,6 +15,7 @@ class HomeViewController: UIViewController, GetStoryboard {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.setCollectionView()
     }
 
     @IBAction private func signOut(_ sender: Any) {
@@ -34,4 +35,40 @@ class HomeViewController: UIViewController, GetStoryboard {
     @IBAction private func add(_ sender: Any) {
     }
 
+    private func setCollectionView() {
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.register(UINib(nibName: "HomeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "HomeCollectionViewCell")
+
+        let cellWidth = collectionView.frame.width - 80
+        let cellHeight = collectionView.frame.height
+
+        // 상하, 좌우 inset value 설정
+        let insetX = (collectionView.bounds.width - cellWidth) / 2.0
+        let insetY = (collectionView.bounds.height - cellHeight) / 2.0
+
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        layout.minimumLineSpacing = 20
+        collectionView.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: insetY, right: insetX)
+
+        // 스크롤이 빠르게 감속되도록 설정
+        collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+    }
+extension HomeViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionat section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionViewCell", for: indexPath)
+        return cell
+    }
+}
 }
