70f812f3dc175bc20a3373f76fbe550586bdaa16
JUNHYEOK KWON <>
2020-01-25 13:55:07 +0900

    AddGoods 화면 베이스 파일 작성

---
+++ Project_2Q_2019/Project_2Q_2019/Screen/AddGoods/AddGoodsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddGoodsViewController.swift
+//  Project_2Q_2019
+//
+//  Created by JUNHYEOK KWON on 2020/01/25.
+//  Copyright © 2020 JUNHYEOK KWON. All rights reserved.
+//
+
+import UIKit
+
+class AddGoodsViewController: UIViewController, GetStoryboard {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Project_2Q_2019/Project_2Q_2019/Screen/AddGoods/AddGoodsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AddGoodsViewModel.swift
+//  Project_2Q_2019
+//
+//  Created by JUNHYEOK KWON on 2020/01/25.
+//  Copyright © 2020 JUNHYEOK KWON. All rights reserved.
+//
+
+import Foundation
+
+struct AddGoodsViewModel {
+}
