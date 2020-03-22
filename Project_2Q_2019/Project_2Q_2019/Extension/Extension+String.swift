32a2bfeeebb1f0151b03a341b0394790f05ce435
JUNHYEOK KWON <>
2020-03-22 16:31:03 +0900

    스트링을 데이트형으로 바꾸는 익스텐션 추가

---
+++ Project_2Q_2019/Project_2Q_2019/Extension/Extension+String.swift
@@ -0,0 +1,48 @@
+//
+//  Extension+String.swift
+//  Project_2Q_2019
+//
+//  Created by JUNHYEOK KWON on 2020/03/22.
+//  Copyright © 2020 JUNHYEOK KWON. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    func getYearText() -> String {
+        guard self.count == 8 else { return "" }
+
+        let startIndex = self.startIndex
+        let lastIndex = self.index(self.startIndex, offsetBy: 3)
+
+        return String(self[startIndex...lastIndex])
+    }
+
+    func getMonthText() -> String {
+        guard self.count == 8 else { return "" }
+
+        let startIndex = self.index(self.startIndex, offsetBy: 4)
+        let lastIndex = self.index(self.startIndex, offsetBy: 5)
+
+        return String(self[startIndex...lastIndex])
+    }
+
+    func getDateText() -> String {
+        guard self.count == 8 else { return "" }
+
+        let startIndex = self.index(self.startIndex, offsetBy: 6)
+        let lastIndex = self.index(self.startIndex, offsetBy: 7)
+
+        return String(self[startIndex...lastIndex])
+    }
+
+    func yyyyMMddToDate() -> Date {
+        guard let year = Int(self.getYearText()),
+            let month = Int(self.getMonthText()),
+            let day = Int(self.getDateText()),
+            let date = DateComponents(calendar: Calendar.current, year: year, month: month, day: day).date else { return Date() }
+
+        return date
+    }
+}
