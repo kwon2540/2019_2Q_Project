c4205a370195abd97900c8d891268606ecac609f
JUNHYEOK KWON <>
2020-03-29 15:58:18 +0900

    일부 홈뷰모델 코드 홈콜렉션뷰모델로 이동, 셀의 바인드 코드 작성

--- Project_2Q_2019/Project_2Q_2019/Screen/Home/ViewParts/HomeCollectionViewCell.swift
+++ Project_2Q_2019/Project_2Q_2019/Screen/Home/ViewParts/HomeCollectionViewCell.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import RxSwift
 
 final class HomeCollectionViewCell: UICollectionViewCell {
-
+    
     @IBOutlet private weak var mainView: UIView!
 
+    private let disposeBag = DisposeBag()
+
+    var viewModel: HomeCollectionViewModel!
+
     override func awakeFromNib() {
+
+    }
+
+    override func layoutSubviews() {
         setLayout()
+
+        bindViewModel()
+        viewModel.loadGoodsFromFirebase()
     }
 
     private func setLayout() {
@@ -22,5 +34,35 @@ final class HomeCollectionViewCell: UICollectionViewCell {
         layer.shadowOpacity = 0.3
         layer.shadowOffset = CGSize(width: 5, height: 5)
         clipsToBounds = false
+    }
+
+    private func bindViewModel() {
+
+        // Output
+        viewModel.apiState.emit(onNext: { [weak self] (state) in
+            guard let this = self else { return }
+
+            switch state {
+            // 로딩 시 인디케이터 표시
+            case .loading:
+                ActivityIndicator.shared.start(view: this.mainView)
+            // 성공시 인디케이터 중지 및 디스미스
+            case .success:
+                this.bindUI()
+                ActivityIndicator.shared.stop(view: this.mainView)
+            // 실패시 드롭다운 표시 및 에러 핸들링 인디케이터 중지
+            case .failed(let error):
+                DropDownManager.shared.showDropDownNotification(view: this.mainView,
+                                                                width: nil,
+                                                                height: nil,
+                                                                type: .error,
+                                                                message: error.description)
+                apiErrorLog(logMessage: error.description)
+                ActivityIndicator.shared.stop(view: this.mainView)
+            }
+        }).disposed(by: disposeBag)
+    }
+
+    private func bindUI() {
     }
 }
