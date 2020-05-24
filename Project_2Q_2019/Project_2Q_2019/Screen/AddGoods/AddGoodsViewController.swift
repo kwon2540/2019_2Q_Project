e22083682232ea9b6c96c672a8d89ac27887821f
JUNHYEOK KWON <>
2020-05-24 16:43:29 +0900

    키보드 공간 작업

--- Project_2Q_2019/Project_2Q_2019/Screen/AddGoods/AddGoodsViewController.swift
+++ Project_2Q_2019/Project_2Q_2019/Screen/AddGoods/AddGoodsViewController.swift
@@ -20,6 +20,7 @@ final class AddGoodsViewController: UIViewController, StoryboardInstantiable {
     @IBOutlet private weak var mainView: UIView!
     @IBOutlet private weak var nameTextField: UITextField!
     @IBOutlet private weak var addButton: RoundButton!
+    @IBOutlet private weak var keyboardSpaceConstraint: NSLayoutConstraint!
 
     private let disposeBag = DisposeBag()
 
@@ -37,15 +38,15 @@ final class AddGoodsViewController: UIViewController, StoryboardInstantiable {
         bindViewModel()
     }
 
-    //    override func viewWillAppear(_ animated: Bool) {
-    //        super.viewWillAppear(animated)
-    //        addKeyboardObservers()
-    //    }
-    //
-    //    override func viewWillDisappear(_ animated: Bool) {
-    //        super.viewWillDisappear(animated)
-    //        removeKeyboardObservers()
-    //    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardObservers()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObservers()
+    }
 
     @IBAction private func dismiss(_ sender: Any) {
         dismissed?()
@@ -127,40 +128,40 @@ extension AddGoodsViewController: UITextFieldDelegate {
 // MARK: Keyboard Notifications
 extension AddGoodsViewController {
 
-    //    private func addKeyboardObservers() {
-    //        let notificationCenter = NotificationCenter.default
-    //        notificationCenter.addObserver(self,
-    //                                       selector: #selector(handleContentUnderKeyboard(notification:)),
-    //                                       name: UIResponder.keyboardWillHideNotification, object: nil)
-    //        notificationCenter.addObserver(self,
-    //                                       selector: #selector(handleContentUnderKeyboard(notification:)),
-    //                                       name: UIResponder.keyboardWillChangeFrameNotification,
-    //                                       object: nil)
-    //    }
-    //
-    //    private func removeKeyboardObservers() {
-    //        let notificationCenter = NotificationCenter.default
-    //        notificationCenter.removeObserver(self)
-    //    }
-    //
-    //    @objc private func handleContentUnderKeyboard(notification: Notification) {
-    //        guard let userInfo = notification.userInfo,
-    //            let keyboardEndFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-    //
-    //        switch notification.name {
-    //        case UIResponder.keyboardWillHideNotification:
-    //            moveContentForDismissKeyboard()
-    //        default:
-    //            let convertedKeyboardEndFrame = view.convert(keyboardEndFrame.cgRectValue, to: view.window)
-    //            moveContent(forKeyboardFrame: convertedKeyboardEndFrame)
-    //        }
-    //    }
-    //
-    //    private func moveContentForDismissKeyboard() {
-    //        scrollViewBottomContraints.constant = 0
-    //    }
-    //
-    //    private func moveContent(forKeyboardFrame keyboardFrame: CGRect) {
-    //        scrollViewBottomContraints.constant = keyboardFrame.height
-    //    }
+    private func addKeyboardObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(handleContentUnderKeyboard(notification:)),
+                                       name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self,
+                                       selector: #selector(handleContentUnderKeyboard(notification:)),
+                                       name: UIResponder.keyboardWillChangeFrameNotification,
+                                       object: nil)
+    }
+
+    private func removeKeyboardObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.removeObserver(self)
+    }
+
+    @objc private func handleContentUnderKeyboard(notification: Notification) {
+        guard let userInfo = notification.userInfo,
+            let keyboardEndFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        switch notification.name {
+        case UIResponder.keyboardWillHideNotification:
+            moveContentForDismissKeyboard()
+        default:
+            let convertedKeyboardEndFrame = view.convert(keyboardEndFrame.cgRectValue, to: view.window)
+            moveContent(forKeyboardFrame: convertedKeyboardEndFrame)
+        }
+    }
+
+    private func moveContentForDismissKeyboard() {
+        keyboardSpaceConstraint.constant = 0
+    }
+
+    private func moveContent(forKeyboardFrame keyboardFrame: CGRect) {
+        keyboardSpaceConstraint.constant = keyboardFrame.height
+    }
 }
