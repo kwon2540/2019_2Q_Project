6cc9200cd5d3a455e02293ad3e08c39c9b26fe65
Maharjan Binish <binish@fenrir.co.jp>
2020-06-22 17:13:09 +0900

    adding files for history content view

---
+++ Project_2Q_2019/Project_2Q_2019/Screen/History/HistoryContentView/HistoryContentViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HistoryContentViewModel.swift
+//  Project_2Q_2019
+//
+//  Created by Maharjan Binish on 2020/06/22.
+//  Copyright © 2020 JUNHYEOK KWON. All rights reserved.
+//
+
+import Foundation
