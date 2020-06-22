444bcba250f400e8f89019a55abb551b997b21ca
Maharjan Binish <binish@fenrir.co.jp>
2020-06-22 15:40:52 +0900

    adding files for cell

---
+++ Project_2Q_2019/Project_2Q_2019/Screen/History/HistoryContentView/Cell/HistoryContentViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryContentViewCell.swift
+//  Project_2Q_2019
+//
+//  Created by Maharjan Binish on 2020/06/22.
+//  Copyright © 2020 JUNHYEOK KWON. All rights reserved.
+//
+
+import UIKit
+
+class HistoryContentViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
