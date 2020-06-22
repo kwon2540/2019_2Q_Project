e0cc9b3b6fee9ca14c868d1a5874c60a22b57272
クォンジュンヒョク <j.kwon@fenrir.co.jp>
2020-06-22 16:17:51 +0900

    Add base file

---
+++ Project_2Q_2019/Project_2Q_2019/Screen/EditGoods/EditGoodsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EditGoodsViewController.swift
+//  Project_2Q_2019
+//
+//  Created by Kwon junhyeok on 2020/06/22.
+//  Copyright © 2020 JUNHYEOK KWON. All rights reserved.
+//
+
+import UIKit
+
+class EditGoodsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Project_2Q_2019/Project_2Q_2019/Screen/EditGoods/EditGoodsViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  EditGoodsViewModel.swift
+//  Project_2Q_2019
+//
+//  Created by Kwon junhyeok on 2020/06/22.
+//  Copyright © 2020 JUNHYEOK KWON. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+
+final class EditGoodsViewModel: APIStateProtocol {
+    
+    let apiStateRelay = PublishRelay<APIState>()
+    
+}
