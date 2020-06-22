fa10cecd710ab7b20b91f8f90c681f70ea970570
Maharjan Binish <binish@fenrir.co.jp>
2020-06-22 19:25:54 +0900

    confirming to tableview delegate and datasource

--- Project_2Q_2019/Project_2Q_2019/Screen/History/HistoryContentView/HistoryContentView.swift
+++ Project_2Q_2019/Project_2Q_2019/Screen/History/HistoryContentView/HistoryContentView.swift
@@ -18,6 +18,64 @@ final class HistoryContentView: UIView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+    // MARK: Setup
+    private func setup() {
+        setupTableView()
+    }
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.registerXib(of: HistoryContentViewCell.self)
+    }
     }
 }
+
+extension HistoryContentView: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.sectionCount
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows(in: section)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueCell(of: HistoryContentViewCell.self, for: indexPath)
+        let cellViewModel = viewModel.viewModelForRow(at: indexPath)
+
+        cell.selectionStyle = .none
+        cell.bind(viewModel: cellViewModel)
+
+        return cell
+    }
+
+}
+
+extension HistoryContentView: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = HistoryContentHeaderView.loadXib()
+        let headerViewModel = viewModel.sectionHeaderViewModel(for: section)
+        headerView.bind(viewModel: headerViewModel)
+        return headerView
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = HistoryContentFooterView.loadXib()
+        return footerView
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 52
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 22
+    }
+}
+
 extension HistoryContentView: XibInstantiable { }
