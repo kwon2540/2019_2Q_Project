f4fbf8d3667eb4d8dcd3205d438fc8966bf510b9
Maharjan Binish <binish@fenrir.co.jp>
2020-06-22 16:49:58 +0900

    creating view model for the section header

--- Project_2Q_2019/Project_2Q_2019/Screen/History/HistoryContentView/Cell/Header/HistoryContentHeaderViewModel.swift
+++ Project_2Q_2019/Project_2Q_2019/Screen/History/HistoryContentView/Cell/Header/HistoryContentHeaderViewModel.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 JUNHYEOK KWON. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct HistoryContentHeaderViewModel {
+
+    private var boughtGoods: [BoughtGoods]
+    private var category: GoodsCategory
+
+    init(boughtGoods: [BoughtGoods], category: GoodsCategory) {
+        self.boughtGoods = boughtGoods
+        self.category = category
+    }
+
+    var totalAmount: String {
+        let amount: Int = boughtGoods.reduce(0) { $0 + ($1.price * $1.amount) }
+        return "\(amount)¥"
+    }
+
+    var icon: UIImage? {
+        return category.image
+    }
+}
