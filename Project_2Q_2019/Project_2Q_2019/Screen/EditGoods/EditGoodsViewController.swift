1dafffe349bd10b40387933871e0f21777f6f379
クォンジュンヒョク <j.kwon@fenrir.co.jp>
2020-06-23 18:13:19 +0900

    Add keyboard handling

--- Project_2Q_2019/Project_2Q_2019/Screen/EditGoods/EditGoodsViewController.swift
+++ Project_2Q_2019/Project_2Q_2019/Screen/EditGoods/EditGoodsViewController.swift
@@ -9,9 +9,57 @@
 import UIKit
 
 class EditGoodsViewController: UIViewController {
+    @IBOutlet private weak var keyboardSpaceConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardObservers()
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObservers()
+    }
+        priceTextField.becomeFirstResponder()
+// MARK: Keyboard Notifications
+extension EditGoodsViewController {
+
+    private func addKeyboardObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(handleContentUnderKeyboard(notification:)),
+                                       name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self,
+                                       selector: #selector(handleContentUnderKeyboard(notification:)),
+                                       name: UIResponder.keyboardWillChangeFrameNotification,
+                                       object: nil)
+    }
+
+    private func removeKeyboardObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.removeObserver(self)
+    }
+
+    @objc private func handleContentUnderKeyboard(notification: Notification) {
+        guard let userInfo = notification.userInfo,
+            let keyboardEndFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        switch notification.name {
+        case UIResponder.keyboardWillHideNotification:
+            moveContentForDismissKeyboard()
+        default:
+            let convertedKeyboardEndFrame = view.convert(keyboardEndFrame.cgRectValue, to: view.window)
+            moveContent(forKeyboardFrame: convertedKeyboardEndFrame)
+        }
+    }
+
+    private func moveContentForDismissKeyboard() {
+        keyboardSpaceConstraint.constant = 0
+    }
+
+    private func moveContent(forKeyboardFrame keyboardFrame: CGRect) {
+        keyboardSpaceConstraint.constant = keyboardFrame.height
     }
 }
