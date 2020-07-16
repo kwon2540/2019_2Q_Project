df611f903104d627a1bd088d8652c3a8b926fcef
クォンジュンヒョク <j.kwon@fenrir.co.jp>
2020-07-16 17:41:56 +0900

    Add Color

--- Project_2Q_2019/Project_2Q_2019/Extension/Extension+UIColor.swift
+++ Project_2Q_2019/Project_2Q_2019/Extension/Extension+UIColor.swift
@@ -17,6 +17,7 @@ extension UIColor {
     static let c859EFF_50 = UIColor(named: "c859EFF@50")!
     static let cF96E4C = UIColor(named: "cF96E4C")!
     static let c6889FF = UIColor(named: "c6889FF")!
+    static let c6889FF_25 = UIColor(named: "c6889FF@25")!
     static let c3F3E3E = UIColor(named: "c3F3E3E")!
     static let cFFA700 = UIColor(named: "cFFA700")!
     static let cFF536F = UIColor(named: "cFF536F")!
@@ -26,6 +27,10 @@ extension UIColor {
     static let c2F2F2F = UIColor(named: "c2F2F2F")!
     static let c3B3B3B = UIColor(named: "c3B3B3B")!
     static let cF3F3F3 = UIColor(named: "cF3F3F3")!
+    static let c7D7D7D = UIColor(named: "c7D7D7D")!
+    static let c888888 = UIColor(named: "c888888")!
+    static let c44CB97 = UIColor(named: "c44CB97")!
+    static let c44CB97_25 = UIColor(named: "c44CB97@25")!
     /// Life
     static let cFEBA5B = UIColor(named: "cFEBA5B")!
     /// Fashion
