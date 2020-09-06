ab78d3f3d803db70fc61e592af0a296274b9a2cf
クォンジュンヒョク <j.kwon@fenrir.co.jp>
2020-09-06 13:33:37 +0900

    prototype

--- Project_2Q_2019/Project_2Q_2019/Screen/Graph/GraphViewController.swift
+++ Project_2Q_2019/Project_2Q_2019/Screen/Graph/GraphViewController.swift
@@ -10,12 +10,49 @@ import UIKit
 
 final class GraphViewController: UIViewController, StoryboardInstantiable {
 
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerXib(of: VerticalGraphCell.self)
     }
 
     @IBAction private func dismiss(_ sender: Any) {
         dismiss(animated: true)
+    }
+}
+
+extension GraphViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension GraphViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueCell(of: VerticalGraphCell.self, for: indexPath)
+        cell.set(graphType: .month, expenditure: "40,000", dateNumber: "3", DateCharacter: "Mar", graphHeight:  0)
+        
+        return cell
+    }
+}
+
+extension GraphViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 50, height: collectionView.frame.height)
     }
 }
