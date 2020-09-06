dee4596d6711744547e62b3048c9bce6005c912b
クォンジュンヒョク <j.kwon@fenrir.co.jp>
2020-09-06 14:31:57 +0900

    Add setup, bind, fetch

--- Project_2Q_2019/Project_2Q_2019/Screen/Graph/GraphViewController.swift
+++ Project_2Q_2019/Project_2Q_2019/Screen/Graph/GraphViewController.swift
@@ -7,21 +7,63 @@
 //
 
 import UIKit
+import RxSwift
 
 final class GraphViewController: UIViewController, StoryboardInstantiable {
 
     @IBOutlet private weak var collectionView: UICollectionView!
     
+    private let hud: ProgressHUD = ProgressHUD.loadXib()
+    private let disposeBag: DisposeBag = DisposeBag()
+    
+    var viewModel: GraphViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setup()
+        bind()
+        fetch()
+    }
+    
+    private func setup() {
         setupCollectionView()
+    }
+    
+    private func bind() {
+        bindApiState()
+    }
+    
+    private func fetch() {
+        viewModel.loadBoughtGoods()
     }
     
     private func setupCollectionView() {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.registerXib(of: VerticalGraphCell.self)
+    }
+    
+    private func bindApiState() {
+        viewModel.apiState.emit(onNext: { [weak self] (state) in
+            guard let this = self else { return }
+
+            switch state {
+            case .loading:
+                this.hud.startAnimation()
+            case .success:
+                this.hud.stopAnimation()
+                this.collectionView.reloadData()
+            case .failed(let error):
+                DropDownManager.shared.showDropDownNotification(view: this.view,
+                                                                width: nil,
+                                                                height: nil,
+                                                                type: .error,
+                                                                message: error.description)
+                apiErrorLog(logMessage: error.description)
+                this.hud.stopAnimation()
+            }
+        }).disposed(by: disposeBag)
     }
 
     @IBAction private func dismiss(_ sender: Any) {
@@ -32,7 +74,7 @@ final class GraphViewController: UIViewController, StoryboardInstantiable {
 extension GraphViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
+        print("didSelectItemAt")
     }
 }
 
@@ -44,7 +86,8 @@ extension GraphViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueCell(of: VerticalGraphCell.self, for: indexPath)
-        cell.set(graphType: .month, expenditure: "40,000", dateNumber: "3", DateCharacter: "Mar", graphHeight:  0)
+        cell.calculateMaxHeightIfNeeded()
+        cell.set(graphType: .date, expenditure: "40,000", dateNumber: "3", DateCharacter: "Mar", heightRatio:  0)
         
         return cell
     }
