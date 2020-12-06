691af9ce9af63b1a30849146940c748818a46811
Maharjan Binish <binish@fenrir.co.jp>
2020-12-06 13:22:44 +0900

    Added Filess for editboughtgoods view controller

---
+++ Project_2Q_2019/Project_2Q_2019/Screen/EditBoughtGoods/EditBoughtGoodsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  EditBoughtGoodsViewModel.swift
+//  Project_2Q_2019
+//
+//  Created by Maharjan Binish on 2020/11/29.
+//  Copyright © 2020 JUNHYEOK KWON. All rights reserved.
+//
+
+import Foundation
