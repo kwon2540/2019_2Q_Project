f73728bffe4c82bd6b25b58f25b4f61c4f3c1d18
Maharjan Binish <binish@fenrir.co.jp>
2020-12-06 13:24:05 +0900

    createed outlets for the editboughtgoodsvc

--- Project_2Q_2019/Project_2Q_2019/Screen/EditBoughtGoods/EditBoughtGoodsViewController.swift
+++ Project_2Q_2019/Project_2Q_2019/Screen/EditBoughtGoods/EditBoughtGoodsViewController.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2020 JUNHYEOK KWON. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class EditBoughtGoodsViewController: UIViewController, StoryboardInstantiable {
+    
+    @IBOutlet private weak var mainView: UIView!
+    @IBOutlet private weak var removeButton: UIButton!
+    @IBOutlet private weak var completeButton: UIButton!
+    @IBOutlet private weak var revertButton: UIButton!
+    @IBOutlet private weak var lifeButton: UIButton!
+    @IBOutlet private weak var fashionButton: UIButton!
+    @IBOutlet private weak var hobbiesButton: UIButton!
+    @IBOutlet private weak var miscellaneousButton: UIButton!
+    @IBOutlet private weak var nameTextField: UITextField!
+    @IBOutlet private weak var nameSaperator: UIView!
+    @IBOutlet private weak var amountTextField: UITextField!
+    @IBOutlet private weak var amountSaperator: UIView!
+    @IBOutlet private weak var priceTextField: UITextField!
+    @IBOutlet private weak var priceSaperator: UIView!
+    @IBOutlet private weak var keyboardSpaceConstraint:NSLayoutConstraint!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
