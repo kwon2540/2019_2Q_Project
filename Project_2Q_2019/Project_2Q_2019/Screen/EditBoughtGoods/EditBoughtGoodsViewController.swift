92876323a91a66acd7a167c6273d5804e6251052
Maharjan Binish <binish@fenrir.co.jp>
2020-12-06 13:40:45 +0900

    implementing keyboard notifications

--- Project_2Q_2019/Project_2Q_2019/Screen/EditBoughtGoods/EditBoughtGoodsViewController.swift
+++ Project_2Q_2019/Project_2Q_2019/Screen/EditBoughtGoods/EditBoughtGoodsViewController.swift
@@ -27,8 +27,51 @@ final class EditBoughtGoodsViewController: UIViewController, StoryboardInstantia
     @IBOutlet private weak var keyboardSpaceConstraint:NSLayoutConstraint!
     
     
+    @IBOutlet private weak var keyboardSpaceConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardObservers()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObservers()
+    }
+// MARK: Keyboard Notifications
+extension EditBoughtGoodsViewController {
+
+    private func addKeyboardObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(handleContentUnderKeyboard(notification:)),
+                                       name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self,
+                                       selector: #selector(handleContentUnderKeyboard(notification:)),
+                                       name: UIResponder.keyboardWillChangeFrameNotification,
+                                       object: nil)
+    }
+
+    private func removeKeyboardObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.removeObserver(self)
+    }
+
+    @objc private func handleContentUnderKeyboard(notification: Notification) {
+        guard let userInfo = notification.userInfo,
+            let keyboardEndFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        switch notification.name {
+        case UIResponder.keyboardWillHideNotification:
+            moveContentForDismissKeyboard()
+        default:
+            let convertedKeyboardEndFrame = view.convert(keyboardEndFrame.cgRectValue, to: view.window)
+            moveContent(forKeyboardFrame: convertedKeyboardEndFrame)
+        }
+    }
+
+    private func moveContentForDismissKeyboard() {
     }
 }
