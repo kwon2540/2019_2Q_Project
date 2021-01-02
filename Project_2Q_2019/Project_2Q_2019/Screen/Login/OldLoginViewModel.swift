1ace9e569216b94b54d8d19c5a3af5cefe2b588a
クォンジュンヒョク <j.kwon@fenrir.co.jp>
2021-01-02 14:55:27 +0900

    既存のLoginViewControllerをOldに変更（メールログインをなくすため）

---
+++ Project_2Q_2019/Project_2Q_2019/Screen/Login/OldLoginViewModel.swift
@@ -0,0 +1,66 @@
+//
+//  LoginViewModel.swift
+//  Project_2Q_2019
+//
+//  Created by JUNHYEOK KWON on 2019/09/23.
+//  Copyright © 2019 JUNHYEOK KWON. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+// MARK: AccountLoginStateProtocol
+protocol AccountLoginStateProtocol {
+
+    var emailText: String { get }
+    var passwordText: String { get }
+}
+
+extension AccountLoginStateProtocol {
+
+    var isEmailValid: Bool {
+        return emailText.contains("@") && emailText.contains(".")
+    }
+
+    var isPasswordValid: Bool {
+        return passwordText.count > 5
+    }
+
+    var isLoginEnabled: Bool {
+        return isPasswordValid && isEmailValid
+    }
+}
+
+struct OldLoginViewModel: APIStateProtocol {
+
+    struct UIState: AccountLoginStateProtocol {
+
+        let emailText: String
+        let passwordText: String
+    }
+
+    let apiStateRelay = PublishRelay<APIState>()
+
+    let emailText = BehaviorRelay(value: "")
+    let passwordText = BehaviorRelay(value: "")
+
+    let isEmailValid: Observable<Bool>
+    let isPasswordValid: Observable<Bool>
+    let isLoginEnabled: Observable<Bool>
+
+    init() {
+        let state = Observable
+            .combineLatest(emailText, passwordText) { UIState(emailText: $0, passwordText: $1) }
+        isEmailValid = state.map { $0.isEmailValid }
+        isPasswordValid = state.map { $0.isPasswordValid }
+        isLoginEnabled = state.map { $0.isLoginEnabled }
+    }
+
+    func login() {
+        apiStateRelay.accept(.loading)
+        FirebaseManager.shared.signIn(email: emailText.value, password: passwordText.value) { (state) in
+            self.apiStateRelay.accept(state)
+        }
+    }
+}
